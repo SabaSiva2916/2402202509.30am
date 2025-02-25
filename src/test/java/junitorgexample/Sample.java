be63b75eb4dc53e41736fe97e6480bba5da26156
SabaSiva2916 <sabasiva2916@gmail.com>
2025-02-25 09:49:31 +0530

    facebook added

--- src/test/java/junitorgexample/Sample.java
+++ src/test/java/junitorgexample/Sample.java
@@ -1,8 +1,11 @@
 package junitorgexample;
 
-
 import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sample {
 
@@ -35,4 +38,15 @@ public class Sample {
 		System.out.println("Method 5");
 	}
 
+	@Test
+	public void tc06() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(" ");
+		WebElement txtUserName = driver.findElement(By.id("email"));
+		txtUserName.sendKeys("Welcome!!!");
+
+		WebElement txtPassword = driver.findElement(By.id("pass"));
+		txtPassword.sendKeys("Welcome@123");
+	}
 }
